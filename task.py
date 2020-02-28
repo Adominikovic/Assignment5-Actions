8251d4257693fd0cd85f9ef935d7a25c69badf9c
Antonio Dominikovic <antoniodomini@hotmail.com>
2020-02-27 19:26:47 -0800

    added firstandlast function and unit test

--- task.py
+++ task.py
@@ -7,3 +7,7 @@ def firstrun():
 
 def circlearea(r):
     return math.pi*(r**2)
+
+
+def firstandlast(some_list):
+    return [some_list[0], some_list[-1]]
--- tests.py
+++ tests.py
@@ -19,6 +19,12 @@ class TestCase(unittest.TestCase):
         area = math.pi*(r*r)
         self.assertEqual(area, task.circlearea(r))
 
+    def test4(self):
+        r = random.randint(2, 100)
+        random_list = random.sample(range(0, 100), r)
+        first_last = [random_list[0], random_list[-1]]
+        self.assertEqual(first_last, task.firstandlast(random_list))
+
 
 if __name__ == '__main__':
     unittest.main()
