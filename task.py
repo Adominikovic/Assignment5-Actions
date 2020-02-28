5d2d9c29e0ec9bc504c5fa262db8094bac0c3f5d
Antonio Dominikovic <antoniodomini@hotmail.com>
2020-02-27 20:25:52 -0800

    added numdays function and unit test

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import math
+import datetime
 
 
 def firstrun():
@@ -11,3 +12,7 @@ def circlearea(r):
 
 def firstandlast(some_list):
     return [some_list[0], some_list[-1]]
+
+
+def numdays(date1, date2):
+    return (date1-date2).days
--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import task
 import random
 import math
+import datetime
 
 
 class TestCase(unittest.TestCase):
@@ -24,6 +25,18 @@ class TestCase(unittest.TestCase):
         random_list = random.sample(range(0, 100), r)
         first_last = [random_list[0], random_list[-1]]
         self.assertEqual(first_last, task.firstandlast(random_list))
+
+    def test5(self):
+        day1 = random.randint(1, 28)
+        day2 = random.randint(1, 28)
+        month1 = random.randint(1, 12)
+        month2 = random.randint(1, 12)
+        year1 = random.randint(1900, 2019)
+        year2 = random.randint(1900, 2019)
+        date1 = datetime.datetime(year1, month1, day1)
+        date2 = datetime.datetime(year2, month2, day2)
+        days = (date1 - date2).days
+        self.assertEqual(days, task.numdays(date1, date2))
 
 
 if __name__ == '__main__':
